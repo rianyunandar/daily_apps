cb247c31d12854824e1e33fb507a0ea5ac29344e
rianyunandar <rianyunandar@gmail.com>
2024-01-04 11:34:52 +0700

    add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,17 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+  // theme: {
+  //   extend: {
+  //     backgroundImage: {
+  //       'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  //       'gradient-conic':
+  //         'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  //     },
+  //   },
+  // },
+  plugins: [require("flowbite/plugin")],
 }
